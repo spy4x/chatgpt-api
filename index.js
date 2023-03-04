3bf89fff71ff13a49ee03e7c2d5875f51346da64
Anton Shubin <2spy4x@gmail.com>
2023-03-04 17:21:46 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import { config } from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY, // don't forget to create ".env" file with the line "OPENAI_API_KEY=your_secret_key here"
+});
+const openai = new OpenAIApi(configuration);
+
+async function chat(prompt) {
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "system", content: "Answer as a pirate" },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+  });
+
+  // extract the answer from response
+  const answer = response.data.choices[0].message.content;
+
+  return answer;
+}
+
+const prompt = `Say hello to my YouTube subscribers, 
+it's 20-ish of them yet.`;
+
+console.log(await chat(prompt));
